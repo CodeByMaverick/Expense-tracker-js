6ebde4ac3a15d7e5e802ddb7ba2ba2b7b63d1612
Maverick <codebymaverick@gmail.com>
2026-06-20 15:45:18 +0530

    Read user input values from form fields

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ console.log("JS connected");
 const button = document.querySelector("button")
 console.log(button);
 
-button.addEventListener("click"), function(){
+button.addEventListener("click", function(){
     console.log("Button clicked!");
-}
+    console.log(textInput.value);
+    console.log(numberInput.value);
+});
+const textInput = document.querySelector('input[type = "text"]');
+const numberInput = document.querySelector('input[type = "number"]');
