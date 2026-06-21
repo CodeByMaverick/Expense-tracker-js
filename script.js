29c8f9c9ad1b082a431376a3fc4c5d54e61a4d0a
Maverick <codebymaverick@gmail.com>
2026-06-21 21:51:48 +0530

    Add dynamic transaction creation

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ button.addEventListener("click", function(){
     console.log("Button clicked!");
     console.log(textInput.value);
     console.log(numberInput.value);
+
+    const newLi = document.createElement("li");
+    newLi.textContent = textInput.value + " " + numberInput.value;
+    transactionList.appendChild(newLi);
+
 });
 const textInput = document.querySelector('input[type = "text"]');
 const numberInput = document.querySelector('input[type = "number"]');
+
+const transactionList = document.querySelector("ul");
