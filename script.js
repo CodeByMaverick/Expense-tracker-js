891ab4770c6aa9801be1768a2fdfa135b4223890
Maverick <codebymaverick@gmail.com>
2026-06-24 23:52:34 +0530

    Organize DOM element references

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 console.log("JS connected");
 
 const button = document.querySelector("button")
+const textInput = document.querySelector('input[type = "text"]');
+const numberInput = document.querySelector('input[type = "number"]');
+const transactionList = document.querySelector("ul");
+const newLi = document.createElement("li");
+
+
 console.log(button);
 
 button.addEventListener("click", function(){
     console.log("Button clicked!");
     console.log(textInput.value);
     console.log(numberInput.value);
-
-    const newLi = document.createElement("li");
+    
     newLi.textContent = textInput.value + " " + numberInput.value;
     transactionList.appendChild(newLi);
     textInput.value = "";
     numberInput.value = "";
 
 });
-const textInput = document.querySelector('input[type = "text"]');
-const numberInput = document.querySelector('input[type = "number"]');
-
-const transactionList = document.querySelector("ul");
